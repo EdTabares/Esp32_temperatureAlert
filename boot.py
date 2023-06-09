4c4225153a0c1c4d1ebfa63b48f2e035b25017aa
Edward Andrés Tabares <107210550+EdTabares@users.noreply.github.com>
2023-06-09 11:15:05 -0500

    Update boot.py

--- boot.py
+++ boot.py
@@ -22,11 +22,11 @@ esp.osdebug(None)
 import gc
 gc.collect()
 
-ssid = 'Redmi'
-password = 'facil6789'
+ssid = '#####'
+password = '#####'
 
-phone_number = '+573014292053'
-api_key = '3108133'
+phone_number = '#########'
+api_key = '########'
 message = '%C2%A1%C2%A1Alerta%21%21%20La%20temperatura%20ha%20subido%20a%20m%C3%A1s%20de%2030%C2%B0' #YOUR MESSAGE HERE (URL ENCODED)https://www.urlencoder.io/
 
 station = network.WLAN(network.STA_IF)
